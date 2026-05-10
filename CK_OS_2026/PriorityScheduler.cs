7ca8780af82bcf26e2a0c09d1e5cd298b6cb5e18
nguyenthanhphong30052k6-cmyk <nguyethanhphong30052k6@gmail.com>
2026-05-11 01:21:16 +0700

    Add Scheduler and Priority Algorithm

---
+++ CK_OS_2026/PriorityScheduler.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CK_OS_2026
+{
+    public class PriorityScheduler : Scheduler // đổi internal thành public để dễ hơn nhé
+    {
+        // constructor
+        public PriorityScheduler(List<Process> processes) : base(processes)
+        {
+
+        }
+
+        public override void Run() // code thuật ở trong hàm này nhé
+        {
+            int currentTime = 0;
+            int completedProcesses = 0;
+            int n = processes.Count;
+
+            while (completedProcesses < n) // lặp cho đến khi tất cả tiến trình hoàn thành
+            {
+                Process currentProcess = null; // tạo biến để lưu tiến trình hiện tại
+
+                foreach (Process p in processes) // duyệt qua tất cả các tiến trình
+                {
+                    if (p.ArrivalTime <= currentTime && p.RemainingTime > 0) // nếu tiến trình đã đến và chưa hoàn thành
+                    {
+                        if (currentProcess == null)
+                        {
+                            currentProcess = p; // nếu chưa có tiến trình nào được chọn, chọn tiến trình hiện tại
+
+                        }
+
+                        else if (p.Priority < currentProcess.Priority)
+                        { // nếu tiến trình hiện tại có độ ưu tiên cao hơn
+                            currentProcess = p;
+
+                        }
+
+                        else if (p.Priority == currentProcess.Priority) // nếu độ ưu tiên bằng nhau, chọn tiến trình có thời gian đến sớm hơn
+                        {
+                            if (p.ArrivalTime < currentProcess.ArrivalTime)
+                            {
+                                currentProcess = p;
+                            }
+                        }
+                    }
+                }
+
+
+                if (currentProcess == null)
+                {
+                    currentTime++;
+                    continue;
+                }
+
+                currentProcess.ResponseTime = currentTime - currentProcess.ArrivalTime;
+                currentTime += currentProcess.BurstTime;
+                currentProcess.CompletionTime = currentTime;
+                currentProcess.RemainingTime = 0;
+                currentProcess.CalculateTimes();
+                completedProcesses++;
+
+                /*
+                    Priority Non-Preemptive Scheduling
+
+                    Rule:
+                    - Process có priority nhỏ hơn sẽ được ưu tiên cao hơn
+                    - CPU sẽ chạy process tới hết rồi mới chuyển process khác
+
+                    Steps:
+
+                    1. Tìm process:
+                        - đã tới
+                        - chưa hoàn thành
+                        - có priority cao nhất
+
+                    2. Nếu chưa có process nào tới:
+                        currentTime++
+
+                    3. Chạy process tới khi hoàn thành
+
+                    4. Cập nhật:
+                        - Response Time: thời gian từ lúc process tới đến lúc được CPU chạy lần đầu
+
+                        - Completion Time: thời điểm process hoàn thành
+
+                        - Waiting Time: tổng thời gian process chờ CPU
+
+                        - Turnaround Time:tổng thời gian process ở trong hệ thống (Completion Time - Arrival Time)
+                */
+            }
+        }
+    }
+}
---
+++ CK_OS_2026/Scheduler.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CK_OS_2026
+{
+    public class Process
+    {
+        // các thuộc tính của tiến trình
+        private string id;
+        private int arrivalTime;
+        private int burstTime;
+        private int priority;
+        private int remainingTime;
+
+        // biến để lưu trữ kết quả sau khi thực hiện thuật toán
+        private int waitingTime;
+        private int turnaroundTime;
+        private int completionTime;
+        private int responseTime;
+
+
+        // constructor
+        public Process(string id, int arrivalTime, int burstTime, int priority = 0) // nếu không có priority thì mặc định là 0
+        {
+            this.id = id;
+            this.arrivalTime = arrivalTime;
+            this.burstTime = burstTime;
+            this.priority = priority;
+
+            this.remainingTime = burstTime; // ban đầu remaining time bằng burst time, sau đó remaining time sẽ giảm dần khi tiến trình được thực hiện
+        }
+
+        // getter và setter
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public int ArrivalTime
+        {
+            get { return arrivalTime; }
+            set { arrivalTime = value; }
+        }
+
+        public int BurstTime
+        {
+            get { return burstTime; }
+            set { burstTime = value; }
+        }
+
+        public int Priority
+        {
+            get { return priority; }
+            set { priority = value; }
+        }
+
+        public int RemainingTime
+        {
+            get { return remainingTime; }
+            set { remainingTime = value; }
+        }
+
+        public int WaitingTime
+        {
+            get { return waitingTime; }
+            set { waitingTime = value; }
+        }
+
+        public int TurnaroundTime
+        {
+            get { return turnaroundTime; }
+            set { turnaroundTime = value; }
+        }
+
+        public int CompletionTime
+        {
+            get { return completionTime; }
+            set { completionTime = value; }
+        }
+
+        public int ResponseTime
+        {
+            get { return responseTime; }
+            set { responseTime = value; }
+        }
+
+        // phương thức tính toán chung vì tất cả các thuật toán đều dùng Turnaround Time = Completion Time - Arrival Time, Waiting Time = Turnaround Time - Burst Time
+        public void CalculateTimes()
+        {
+            turnaroundTime = completionTime - arrivalTime;
+            waitingTime = turnaroundTime - burstTime;
+        }
+    }
+
+    public class Scheduler
+    {
+        protected List<Process> processes; // danh sách các tiến trình để thực hiện thuật toán, dùng protected để các lớp con có thể truy cập và thực hiện thuật toán trên danh sách này
+
+        // constructor
+        public Scheduler(List<Process> processes)
+        {
+            this.processes = processes;
+        }
+
+        /*
+            Các thuật toán FCFS, SJF, RR, Priority...
+            sẽ kế thừa lớp Scheduler và override phương thức Run().
+
+            Trong hàm Run():
+            - Thực hiện thuật toán scheduling
+            - Tính Completion Time
+            - Tính Response Time (nếu cần)
+            - Gọi CalculateTimes() cho từng process
+              để tính Waiting Time và Turnaround Time
+
+            Ví dụ:
+
+            public class FCFSScheduler : Scheduler
+            {
+                public FCFSScheduler(List<Process> processes)
+                    : base(processes)
+                {
+
+                }
+
+                public override void Run()
+                {
+                    // code FCFS ở đây nhé
+                }
+            }
+
+            => Tức là mấy ông tách ra một file FCFS.cs để code giao diện cho WinForms, 1 file FCFSScheduler để code thuật toán
+        */
+
+        public virtual void Run() // cho phép các class con có thể ghi đè, ở đây để trống vì class cha không biết các thuật toán chạy cụ thể ra sao
+        {
+            // phương thức sẽ được override ở các thuật toán cụ thể
+        }
+
+        /*
+            Các thuật toán khác như:
+            - FCFS
+            - SJF
+            - Round Robin
+            - Priority
+
+            sẽ kế thừa Scheduler và override phương thức Run().
+
+            Điểm khác nhau chủ yếu:
+            - cách chọn process
+            - cách sắp xếp process
+
+            Công thức tính:
+            - Waiting Time
+            - Turnaround Time
+            - Response Time
+
+            vẫn giống nhau.
+        */
+
+    }
+}
